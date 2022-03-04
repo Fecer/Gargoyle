798390eb75d4a2870b6b69769e29a0a298d789cc
vivianyang11 <92029241+vivianyang11@users.noreply.github.com>
2022-03-04 21:29:09 +0000

    finish NewsScore.java and QueryNewsAVGScore.java

--- BigData-AE/src/uk/ac/gla/dcs/bigdata/studentstructures/NewsScore.java
+++ BigData-AE/src/uk/ac/gla/dcs/bigdata/studentstructures/NewsScore.java
@@ -1,9 +1,50 @@
 package uk.ac.gla.dcs.bigdata.studentstructures;
 
 import java.io.Serializable;
+//import java.util.List;
 
 public class NewsScore implements Serializable{
 
+
+	private static final long serialVersionUID = 2265723765321050131L;
+	
+	String id;
+    double DPHScore;
+    String title;
+    
+    public NewsScore() {};
+	
+	public NewsScore(String id, double DPHScore, String title) {
+		super();
+		this.id = id;
+		this.DPHScore = DPHScore;
+		this.title = title;
+	}
+    
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public double getDPHScore() {
+		return DPHScore;
+	}
+	
+	public void setDPHScore(double DPHScore) {
+		this.DPHScore = DPHScore;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+    
 
 
 }
