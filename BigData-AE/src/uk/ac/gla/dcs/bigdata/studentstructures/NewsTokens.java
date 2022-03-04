548380a39fd398abb2bf5df53c5d09292175edbd
vivianyang11 <92029241+vivianyang11@users.noreply.github.com>
2022-03-04 19:13:50 +0000

    Update NewsTokens.java

--- BigData-AE/src/uk/ac/gla/dcs/bigdata/studentstructures/NewsTokens.java
+++ BigData-AE/src/uk/ac/gla/dcs/bigdata/studentstructures/NewsTokens.java
@@ -1,5 +1,45 @@
 package uk.ac.gla.dcs.bigdata.studentstructures;
 
-public class NewsTokens {
+import java.io.Serializable;
+import java.util.List;
+
+//import uk.ac.gla.dcs.bigdata.providedstructures.ContentItem;
+
+public class NewsTokens implements Serializable{
+	
+	private static final long serialVersionUID = -2374545335021590334L;
+	
+	String id;
+	List<String> tokens;//tokens of title + five (or less)paragraph
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public List<String> getTokens() {
+		return tokens;
+	}
+	
+	public void setTokens(List<String> tokens) {
+		this.tokens = tokens;
+	}
+	
+	
+	
+	
+	public NewsTokens(String id, List<String> tokens) {
+		super();
+		this.id = id;
+		this.tokens = tokens;
+	}
+	
+	
+	
 
 }
+
+
